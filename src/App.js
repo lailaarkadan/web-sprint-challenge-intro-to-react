531a5a51458b15d6d8c4d6348cf0394db069fee0
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-11-05 14:54:32 -0600

    fixed errors

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import './App.css';
 import Character from './components/Character'
 
+
+
 const App = () => {
 
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -12,28 +14,35 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-const [characters, setCharacters] = useState();
-useEffect(() => {
-  axios.get(`https://swapi.dev/api/people/`)
-    .then(res => {
-      setCharacters(res.data.results)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-}, [])
-
-
   
-return (
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+      .then(res => {
+        setCharacters(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      
-   </div>
+    
+      {characters.map((data, info) => {
+        return(
+          <Character key={info} name={data.name} gender={data.gender} height={data.height} mass={data.mass} birth_year={data.birth_year} eye_color={data.eye_color} hair_color={data.hair_color} skin_color={data.skin_color} />
+        )
+      })}
+          </div>
+  
   );
 }
 
 export default App;
+// .map is giving me an error - map not defined
 
 
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,4 +1,32 @@
 // Write your Character component here
-import React from 'react';
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import styled from "styled-components";
+
+
+const Style = styled.div`
+border: 2px solid green;
+margin-left: 20%;
+margin-right:20%;
+h2, p{
+    color: ; black
+    font-family: serif
+}
+`
+export default function Character(data) {
+
+return (
+    <Style className='App'>
+        <h2>{data.name}</h2>    
+        <h4>Gender: {data.gender}</h4>
+        <h4>Height: {data.height}</h4>
+        <h4>Mass: {data.mass}</h4>
+        <h4>BirthYear: {data.birth_year}</h4>
+        <h4>Eye Color: {data.eye_color}</h4>
+        <h4>Hair Color: {data.hair_color}</h4>
+        <h4>Skin Color: {data.skin_color}</h4>
+  </Style>
+)
+}
+
+
 
